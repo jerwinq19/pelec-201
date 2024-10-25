bc54561d7a5a5fc954e21efad9a94272b71284d0
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2024-10-26 01:48:52 +0800

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,4 @@
+document.getElementById('navbar-toggle').addEventListener('click', function() {
+    const buttonsContainer = document.querySelector('.buttons');
+    buttonsContainer.classList.toggle('active'); // Toggle active class to show/hide links
+});
